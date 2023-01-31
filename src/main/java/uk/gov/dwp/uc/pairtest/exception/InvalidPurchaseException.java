9bc563763a36e6d7c1eab81052261c399d0c2a67
slvcode <123327887+slvcode@users.noreply.github.com>
2023-01-31 22:23:20 +0000

    Remove target files

--- src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
+++ src/main/java/uk/gov/dwp/uc/pairtest/exception/InvalidPurchaseException.java
@@ -2,6 +2,7 @@ package uk.gov.dwp.uc.pairtest.exception;
 
 public class InvalidPurchaseException extends RuntimeException {
 
-    public InvalidPurchaseException(String s) {
+    public InvalidPurchaseException(String errorMessage) {
+        super(errorMessage);
     }
 }
